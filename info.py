80a34166f3307a357825fe736132291bbf597d4a
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-16 16:54:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -101,7 +101,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantlink.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '0e6d1c7fea91075af9fc276267b2df1dc6bf9bfb')
 
