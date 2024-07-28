ae29628d4d1ad6dd599439f084d46f93dc1b0354
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-28 19:35:49 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -21,7 +21,7 @@ async def pm_broadcast(bot, message):
         deleted = 0
         failed = 0
         success = 0
-        for user in users:
+        async for user in users:
             if 'id' in user:
                 pti, sh = await broadcast_messages(int(user['id']), b_msg)
                 if pti:
@@ -65,7 +65,7 @@ async def broadcast_group(bot, message):
     failed = 0
 
     success = 0
-    for group in groups:
+    async for group in groups:
         pti, sh = await broadcast_messages_group(int(group['id']), b_msg)
         if pti:
             success += 1
